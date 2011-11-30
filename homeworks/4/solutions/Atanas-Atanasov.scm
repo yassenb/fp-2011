bae103ff7bf4bf00e1dae97488208c5ccfb6bde8
Yassen Bantchev <yassenb@gmail.com>
2011-11-30 22:50:39 +0200

    add more solutions and grades for homework 4

--- homeworks/4/solutions/Atanas-Atanasov.scm
+++ homeworks/4/solutions/Atanas-Atanasov.scm
@@ -1,3 +1,14 @@
+(define (pair_switcher lst)
+    (if (or (null? lst) (null? (cdr lst)))
+        lst
+        (cons (cadr lst) (cons (car lst) (pair_switcher (cdr (cdr lst)))))))
+
+
+(pair_switcher '(1 2 3 4 5))          
+(pair_switcher '(1 2 3 4 5 6))
+(pair_switcher '())
+(pair_switcher '(1))
+                  
 (define my_append (lambda lst
     (define (append_two l1 l2)
       (if (null? l1)
